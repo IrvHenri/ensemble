6b4012d419396d5058116fad550e00fed1e43d07
Irving Henriquez <irving@midnightmarketing.com>
2022-04-27 17:43:04 -0400

    created movie list component and add goggle gonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import ListTitle from "./components/ListTitle";
 import useMoviesData from "./hooks/useMoviesData";
 function App() {
   const [moviesData] = useMoviesData();
   console.log(moviesData);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section>
+        <ListTitle title="Movies" />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ListTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListTitle = ({ title }) => {
+  return <h1 className="list-title">{title}</h1>;
+};
+
+export default ListTitle;
